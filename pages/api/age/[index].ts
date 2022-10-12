4eab91a953b681974c207b0c6f7ab4b47a8706be
ajin15971@gmail.com <ajin15971@gmail.com>
2022-10-12 21:58:43 +0900

    2022-10-12-6

--- pages/api/age/[index].ts
+++ pages/api/age/[index].ts
@@ -23,80 +23,81 @@ export default async function handler(
 
   // console.log(yearNumber);
 
-  switch (yearNumber) {
-    case "2022":
-      House = await prisma.transactions_age_2022.findMany({
-        // 22년도 주택 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "06",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      Apart = await prisma.transactions_age_2022.findMany({
-        // 22년도 아파트 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "07",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      break;
-    case "2021":
-      House = await prisma.transactions_age_2021.findMany({
-        // 22년도 주택 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "06",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      Apart = await prisma.transactions_age_2021.findMany({
-        // 22년도 아파트 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "07",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      break;
-    case "2020":
-      House = await prisma.transactions_age_2020.findMany({
-        // 22년도 주택 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "06",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      Apart = await prisma.transactions_age_2020.findMany({
-        // 22년도 아파트 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "07",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      break;
-    case "2019":
-      House = await prisma.transactions_age_2019.findMany({
-        // 22년도 주택 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "06",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      Apart = await prisma.transactions_age_2019.findMany({
-        // 22년도 아파트 거래
-        where: {
-          REGION_CD: ageRegions,
-          DEAL_OBJ: "07",
-          RESEARCH_DATE: ageDayRegion,
-        },
-      });
-      break;
-  }
+  // switch (yearNumber) {
+  //   case "2022":
+  House = await prisma.transactions_age_2022.findMany({
+    // 22년도 주택 거래
+    where: {
+      REGION_CD: ageRegions,
+      DEAL_OBJ: "06",
+      RESEARCH_DATE: ageDayRegion,
+    },
+  });
+  Apart = await prisma.transactions_age_2022.findMany({
+    // 22년도 아파트 거래
+    where: {
+      REGION_CD: ageRegions,
+      DEAL_OBJ: "07",
+      RESEARCH_DATE: ageDayRegion,
+    },
+  });
+  // break;
+  //   case "2021":
+  //     House = await prisma.transactions_age_2021.findMany({
+  //       // 22년도 주택 거래
+  //       where: {
+  //         REGION_CD: ageRegions,
+  //         DEAL_OBJ: "06",
+  //         RESEARCH_DATE: ageDayRegion,
+  //       },
+  //     });
+  //     Apart = await prisma.transactions_age_2021.findMany({
+  //       // 22년도 아파트 거래
+  //       where: {
+  //         REGION_CD: ageRegions,
+  //         DEAL_OBJ: "07",
+  //         RESEARCH_DATE: ageDayRegion,
+  //       },
+  //     });
+  //     break;
+  //   case "2020":
+  //     House = await prisma.transactions_age_2020.findMany({
+  //       // 22년도 주택 거래
+  //       where: {
+  //         REGION_CD: ageRegions,
+  //         DEAL_OBJ: "06",
+  //         RESEARCH_DATE: ageDayRegion,
+  //       },
+  //     });
+  //     Apart = await prisma.transactions_age_2020.findMany({
+  //       // 22년도 아파트 거래
+  //       where: {
+  //         REGION_CD: ageRegions,
+  //         DEAL_OBJ: "07",
+  //         RESEARCH_DATE: ageDayRegion,
+  //       },
+  //     });
+  //     break;
+  //   case "2019":
+  //     House = await prisma.transactions_age_2019.findMany({
+  //       // 22년도 주택 거래
+  //       where: {
+  //         REGION_CD: ageRegions,
+  //         DEAL_OBJ: "06",
+  //         RESEARCH_DATE: ageDayRegion,
+  //       },
+  //     });
+  //     Apart = await prisma.transactions_age_2019.findMany({
+  //       // 22년도 아파트 거래
+  //       where: {
+  //         REGION_CD: ageRegions,
+  //         DEAL_OBJ: "07",
+  //         RESEARCH_DATE: ageDayRegion,
+  //       },
+  //     });
+  //     break;
+  // }
+
   res.status(200).json({ House, Apart });
 
   // res.status(200).json({
